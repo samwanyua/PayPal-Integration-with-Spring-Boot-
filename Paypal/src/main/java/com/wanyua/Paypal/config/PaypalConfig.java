081a365bb968e82fa426a7a41657f85763e19578
samwanyua <samexample8@gmail.com>
2024-07-09 20:12:52 +0300

    Initial commit

---
+++ Paypal/src/main/java/com/wanyua/Paypal/config/PaypalConfig.java
@@ -0,0 +1,10 @@
+package com.wanyua.Paypal.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaypalConfig {
+    private String clientId;
+    private String clientSecret;
+    private String mode;
+}
